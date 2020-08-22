97e7bc60682393255abea021681559c38ef69d59
lalebdi <loai.alebdi@gmail.com>
2020-08-22 16:20:37 -0400

    added the side bar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import React from 'react';
 import Login from './Views/Login/Login.jsx';
-import { useSelector } from 'react-redux'
+import { useSelector } from 'react-redux';
+import { BrowserRouter as Router } from 'react-router-dom';
+import SideBar from './Views/Sidebar/SideBar.jsx';
 
 function App() {
   const { auth } = useSelector(state => state) // to define the login status
   return (
-    <>
+    <Router>
       { !auth.LoggedIn ? <Login /> :
-      <h1>Hello</h1>}
-    </>
+      <>
+      <SideBar />
+      </>
+      }
+    </Router>
   );
 }
 
--- src/Views/SIdebar/SideBar.jsx
+++ src/Views/SIdebar/SideBar.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { signOut } from '../../Controllers/Redux/authSlice';
+import './sideBar.css';
 
 const SideBar = () => {
     const dispatch = useDispatch();
