973ff299f44651967f1989a79c427c5f5a4ada2b
lalebdi <loai.alebdi@gmail.com>
2020-08-22 12:05:15 -0400

    created the auth slice

--- src/Controllers/Redux/authSlice.js
+++ src/Controllers/Redux/authSlice.js
@@ -1,1 +1,29 @@
 import { createSlice } from '@reduxjs/toolkit'; // will have our reducesers 
+
+const slice = createSlice({
+    name: "auth", 
+    initialState:{
+        admin: false,
+        LoggedIn: false
+    },
+    reducers: {
+        reducers: {
+            signIn:(state,action)=>{
+                const { name, password } = action.payload;
+                state.LoggedIn= true;
+                state.admin = true;
+            },
+            signOut:(state)=>{
+                state.LoggedIn = false;
+                state.admin = false;
+            },
+            createUser:(state,action)=>{
+
+            }
+
+        }
+    }
+})
+
+export default slice.reducer;
+export const { signIn, signOut, createUser } = slice.actions;
