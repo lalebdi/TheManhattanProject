d859b2809f7974fe8300a48f01dba6fe449833cc
lalebdi <loai.alebdi@gmail.com>
2020-08-22 12:32:10 -0400

    created the dummy data in the bug controller

--- src/Controllers/BugController.js
+++ src/Controllers/BugController.js
@@ -1,1 +1,31 @@
-import
+import BugModel from '../Models/BugModel';
+
+export function retrieveBugs(){
+    let data = [];
+
+    data.push(new BugModel({
+        _id: 23456789,
+        name: "Crash on Load",
+        details: "Crashes after 3 seconds",
+        steps: " Open application and it will crash",
+        version: "V2.0",
+        assigned: "Leah",
+        creator: "Natasha",
+        priority: 1,
+        time: "23:38"
+    }))
+    data.push(new BugModel({
+        _id: 23456789,
+        name: "Wont  Load",
+        details: "Crashes after 3 seconds",
+        steps: " Open application and it will crash",
+        version: "V2.0",
+        assigned: "Leah",
+        creator: "Natasha",
+        priority: 3,
+        time: "23:38"
+    }))
+    
+    let sorted = data.sort((a,b) => {return a.priority - b.priority }) // this will go throught the array and sort them by the priority and if its not 0 then it is 0 if higher it will return 1 first.
+    return sorted;
+}
