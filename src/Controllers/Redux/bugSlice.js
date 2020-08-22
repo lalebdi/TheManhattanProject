28090f51af1609bb6ca0cc4dc0344d9c1e8ca330
lalebdi <loai.alebdi@gmail.com>
2020-08-22 12:35:42 -0400

    simplified the get bugs

--- src/Controllers/Redux/bugSlice.js
+++ src/Controllers/Redux/bugSlice.js
@@ -1,12 +1,11 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { retrieveBugs } from '../BugController';
 
 const slice = createSlice({
     name: "bug",
     initialState: [],
     reducers: {
-        getBugs:(state) => {
-
-        },
+        getBugs:state => retrieveBugs() , // anything that is return from here will set the state
         createBugs:(state,actions)=>{
 
         },
