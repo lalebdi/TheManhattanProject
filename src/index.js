0e0aa57eeb2a37065aed4eb3bcae41c9da44c52b
lalebdi <loai.alebdi@gmail.com>
2020-08-22 12:56:37 -0400

    setup the state in the index

--- src/index.js
+++ src/index.js
@@ -3,10 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux';
+import { configureStore, combineReducers } from '@reduxjs/toolkit'; // the reason for the combine reducers is because we have multiple reducers (slices)
+
+// Reducers:
+import authReducer from './Controllers/Redux/authSlice';
+import bugReducer from './Controllers/Redux/bugSlice';
+import userReducer from './Controllers/Redux/UserSlice';
+
+// Redux configure
+const reducer = combineReducers({
+  auth: authReducer,
+  bug: bugReducer,
+  user: userReducer,
+})
+
+// after combining the the reducers I need to configure the store
+const store = configureStore({
+  reducer  // ES6 dont need to do reducer : reducer since they are the same name.
+})
 
 ReactDOM.render(
-  
-    <App />,
+    <Provider store={store}>
+    <App />
+    </Provider>,
   document.getElementById('root')
 );
 
