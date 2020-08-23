a7387ba549384980ea78c69b0fd1b507eed2c937
lalebdi <loai.alebdi@gmail.com>
2020-08-23 12:41:57 -0400

    added the redirect functionality

--- src/Views/Components/Dashboard/Card.jsx
+++ src/Views/Components/Dashboard/Card.jsx
@@ -5,7 +5,7 @@ import './Card.css';
 const Card = (props) => {
     const {level , color} = Priority(props.priority)
     return (
-        <div className="dashboard-card" style={{ color: color}}>
+        <div className="dashboard-card" onClick={props.clicked} style={{ color: color}}>
             <h2>Total: {level} </h2>
             <p>{props.count}</p>
         </div>
--- src/Views/Pages/Dashboard/Dashboard.jsx
+++ src/Views/Pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,13 @@
 import React, { useEffect } from 'react';
 import { useDispatch,useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 import { getBugs } from '../../../Controllers/Redux/bugSlice';
 import Card from '../../Components/Dashboard/Card';
 
 const Dashboard = () => {
     const dispatch = useDispatch();
     const bugs = useSelector(state => state.bugs) // we need to be adble to dispatch them
+    const browserHistory = useHistory(); //this is for the onclick on the card
     let highCount = 0;
     let midCount = 0;
     let lowCount = 0;
@@ -15,6 +17,10 @@ const Dashboard = () => {
         highCount = filterBugs(1);
         midCount = filterBugs(2);
         lowCount = filterBugs(3);
+    }
+
+    function redirect(){
+        browserHistory.push('/viewbugs')
     }
 
     function filterBugs(priority){
@@ -27,9 +33,9 @@ const Dashboard = () => {
     }, [bugs == undefined]) // it will update the state and useEffect runs eveytime the state is updated. So, only runs if the state is undefined.
     return (
         <div className="page-container" >
-            <Card priority="1" count={ highCount.length} />
-            <Card priority="2" count={ midCount.length} />
-            <Card priority="3" count={ lowCount.length} />
+            <Card priority="1" count={ highCount.length} clicked={redirect}/>
+            <Card priority="2" count={ midCount.length} clicked={redirect}/>
+            <Card priority="3" count={ lowCount.length} clicked={redirect}/>
         </div>
     )
 }
