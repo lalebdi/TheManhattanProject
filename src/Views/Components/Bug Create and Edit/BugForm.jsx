e31c1020de8bdd3e8ba29f85991c45620d3d5446
lalebdi <loai.alebdi@gmail.com>
2020-08-23 11:23:17 -0400

    added state

--- src/Views/Components/Bug Create and Edit/BugForm.jsx
+++ src/Views/Components/Bug Create and Edit/BugForm.jsx
@@ -1,29 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './BugForm.css';
+import BugModel from '../../../Models/BugModel';
 
 const BugForm = (props) => {
+    const [ bugObject, setBugObject] = useState(new BugModel(props.bug)) // going to pass in the bug from props. So when we edit the bug so it will set all the input. if its null it will create an empty object
+    
+    function inputChanged(e){
+        setBugObject({ // I need to set this to the previous one
+            ...bugObject, //after, I need to get what input is chnaging it and then get the name from the event triggering it
+            [e.target.name] : e.target.value
+        })
+    }
+    
     return (
         <div className="bug-create">
             <h1>{props.title}</h1>
             <form>
                 <label>Name:</label>
-                <input type="text" name="name" placeholder="Bug Name" required></input>
+                <input type="text" name="name" placeholder="Bug Name" required onChange={inputChanged} value={bugObject.name}></input>
                 <label>Details:</label>
-                <textarea name="details" placeholder="Detailed description on the bug" required></textarea>
+                <textarea name="details" placeholder="Detailed description on the bug" required onChange={inputChanged} value={bugObject.details}></textarea>
                 <label>Steps:</label>
-                <textarea name="steps" placeholder="Steps to create the bug" required></textarea>
+                <textarea name="steps" placeholder="Steps to create the bug" required onChange={inputChanged} value={bugObject.steps}></textarea>
                 <label>Priority</label>
-                <select name="priority" required>
+                <select name="priority" required onChange={inputChanged} value={bugObject.priority} >
                     <option value="1">High</option>
                     <option value="2">Mid</option>
                     <option value="3">Low</option>
                 </select>
                 <label>Assigned</label>
-                <select name="assigned">
+                <select name="assigned" onChange={inputChanged} value={bugObject.assigned}>
                     <option>Leah</option>
                 </select>
                 <label>Application Version:</label>
-                <input name="version" placeholder="Application Version"></input>
+                <input name="version" placeholder="Application Version" onChange={inputChanged} value={bugObject.version}></input>
                 <button type="submit"> {props.title} </button>
             </form>
         </div>
