9f330c4971b5bd6647422da32f71b03f2effcf60
lalebdi <loai.alebdi@gmail.com>
2020-08-22 20:53:21 -0400

    passed the close function and the card

--- src/Views/Pages/ViewBugs.jsx
+++ src/Views/Pages/ViewBugs.jsx
@@ -1,9 +1,14 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { getBugs } from '../../Controllers/Redux/bugSlice';
-import BugCard from '../Bug Card/BugCard'
+import BugCard from '../Components/Bug Card/BugCard';
+import BugView from '../Components/Bug View/BugView';
 
 const ViewBugs = () => {
+    const [DISPLAY_BUG, SET_DISPLAY_BUG] = useState({
+        name: '',
+        isDisplayed: false
+    })
     const dispatch = useDispatch();
     const { bugs } = useSelector(state => state) ;
 
@@ -12,7 +17,10 @@ const ViewBugs = () => {
     }, [bugs.length < 1]) // this is the trigger when bugs is null
 
     function BugClicked(name){
-
+        SET_DISPLAY_BUG({
+            isDisplayed: !DISPLAY_BUG.isDisplayed,
+            name : name,
+        })
     }
 
 
@@ -21,6 +29,7 @@ const ViewBugs = () => {
             {bugs.map((bug,key) =>(
                 <BugCard key={key} bug={bug} clicked={BugClicked}/>
             ))}
+            {DISPLAY_BUG.isDisplayed && <BugView clicked={BugClicked} bug={bugs.filter((bug)=> bug.name === DISPLAY_BUG.name)[0]} />}
         </div>
     )
 }
