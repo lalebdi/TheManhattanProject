c0e4256efba612dc33f5f22a35f98f6485a71ed9
lalebdi <loai.alebdi@gmail.com>
2020-09-16 22:28:50 -0400

    simple modification

--- src/Views/SIdebar/SideBar.jsx
+++ src/Views/SIdebar/SideBar.jsx
@@ -22,7 +22,7 @@ const SideBar = () => {
             <ul>
                 <li><Link to="/" className="nav-link" >Dashboard</Link></li>
                 <li><Link to="/viewbugs" className="nav-link" >View Bugs</Link></li>
-               {auth.admin && <li><Link to="/create" className="nav-link" >Create Bug</Link></li>}
+                {auth.admin && <li><Link to="/create" className="nav-link" >Create Bug</Link></li>}
             </ul>
             <button className="nav-link logout" onClick={Signout}>Logout</button>
         </div>
